833986625b82c8f025352c37c0e0589e6d4d022a
ParkSeongGeun <phd0801@naver.com>
2025-04-07 02:14:50 +0900

    [Feat] add locationManager

--- SeoulMate/PresentationSM/Map/MapViewController.swift
+++ SeoulMate/PresentationSM/Map/MapViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import CoreLocation
 import GoogleMaps
 
 class MapViewController: UIViewController {
   
   // MARK: - Properties
   private let mapView = GMSMapView()
+  private let locationManager = CLLocationManager()
   
   // 건국대학교 좌표
   private let initialLocation = CLLocationCoordinate2D(latitude: 37.540693, longitude: 127.079361)
@@ -20,11 +22,22 @@ class MapViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // 위치 설정
+    setupLocationManager()
+    
     // 지도 초기 설정
     setupMapView()
   }
   
   // MARK: - Setup
+  private func setupLocationManager() {
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    
+    // 위치 권한 요청
+    locationManager.requestWhenInUseAuthorization()
+  }
+  
   private func setupMapView() {
     // 카메라 설정 (건국대학교 중심, 줌 레벨 15)
     let camera = GMSCameraPosition.camera(
@@ -32,19 +45,44 @@ class MapViewController: UIViewController {
       zoom: 15
     )
     
-    // 맵뷰 생성 및 카메라 설정
     mapView.camera = camera
-    
-    // 내 위치 버튼 표시
     mapView.settings.myLocationButton = true
+    mapView.settings.compassButton = true
     
-    // 맵뷰 추가
+    
+    // UI Setting
     view.addSubview(mapView)
     
-    // SnapKit을 사용한 제약조건 설정
     mapView.snp.makeConstraints { make in
       make.edges.equalToSuperview()
     }
   }
 }
 
+// MARK: - CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    switch status {
+    case .authorizedWhenInUse, .authorizedAlways:
+      locationManager.startUpdatingLocation()
+      mapView.isMyLocationEnabled = true
+      mapView.settings.myLocationButton = true
+    case .denied, .restricted:
+      // 권한이 거부된 경우 처리
+      let alert = UIAlertController(
+        title: "위치 권한 필요",
+        message: "내 위치를 표시하려면 위치 권한이 필요합니다. 설정에서 권한을 허용해주세요.",
+        preferredStyle: .alert
+      )
+      alert.addAction(UIAlertAction(title: "설정으로 이동", style: .default) { _ in
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+          UIApplication.shared.open(url)
+        }
+      })
+      alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+      present(alert, animated: true)
+    default:
+      break
+    }
+  }
+}
