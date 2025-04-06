ed4355603cc518b3997b0dd6d16ce5e1bfa7d5b5
ParkSeongGeun <phd0801@naver.com>
2025-04-07 01:44:13 +0900

    Create UIViewController+Extension.swift

---
+++ SeoulMate/PresentationSM/Utils/UIViewController+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+Extension.swift
+//  SeoulMate
+//
+//  Created by 박성근 on 4/7/25.
+//
+
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
