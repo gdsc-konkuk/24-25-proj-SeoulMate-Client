f8ab6faf277ebcfc9be2da06ee74f2b2cda39fda
ParkSeongGeun <phd0801@naver.com>
2025-04-07 01:44:18 +0900

    Create Notification+Extension.swift

---
+++ SeoulMate/PresentationSM/Utils/Notification+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Extension.swift
+//  SeoulMate
+//
+//  Created by 박성근 on 4/7/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let userDidSignIn = Notification.Name("userDidSignIn")
+    static let userDidSignOut = Notification.Name("userDidSignOut")
+}
