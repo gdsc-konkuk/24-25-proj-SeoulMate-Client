2d54ee4c6236fcff74bc8ae5a32b81724bc929b7
ParkSeongGeun <phd0801@naver.com>
2025-04-07 01:44:55 +0900

    Update SocialLoginViewController.swift

--- SeoulMate/PresentationSM/Login/SocialLoginViewController.swift
+++ SeoulMate/PresentationSM/Login/SocialLoginViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import SnapKit
+import GoogleSignIn
+import SwiftUI
 
 final class SocialLoginViewController: UIViewController {
   
@@ -13,8 +16,64 @@ final class SocialLoginViewController: UIViewController {
   private let titleLabel: UILabel = {
     let label = UILabel()
     label.text = "SeoulMate"
-    label.font = .boldFont(ofSize: <#T##CGFloat#>)
+    label.font = .boldFont(ofSize: 36)
     label.textAlignment = .center
     return label
   }()
+  
+  // google login button
+  private let googleSignInButton: GIDSignInButton = {
+    let button = GIDSignInButton()
+    button.style = .wide
+    button.colorScheme = .light
+    return button
+  }()
+  
+  // MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+    setupActions()
+  }
+}
+
+extension SocialLoginViewController {
+  private func setupUI() {
+    view.backgroundColor = .white
+    
+    view.addSubview(titleLabel)
+    view.addSubview(googleSignInButton)
+    
+    titleLabel.snp.makeConstraints { make in
+      make.top.equalToSuperview().offset(UIApplication.screenHeight * 0.3)
+      make.centerX.equalToSuperview()
+    }
+    
+    googleSignInButton.snp.makeConstraints { make in
+      make.bottom.equalToSuperview().offset(-UIApplication.screenHeight * 0.15)
+      make.centerX.equalToSuperview()
+      make.width.equalTo(UIApplication.screenWidth * 0.7)
+    }
+  }
+  
+  private func setupActions() {
+    googleSignInButton.addTarget(self, action: #selector(handleGoogleSignIn), for: .touchUpInside)
+  }
+}
+
+extension SocialLoginViewController {
+  @objc private func handleGoogleSignIn() {
+    GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+      guard error == nil else { return }
+      
+      // If sign in succeeded, display the app's main content View.
+      NotificationCenter.default.post(name: Notification.Name.userDidSignIn, object: nil)
+    }
+  }
+}
+
+struct PreView: PreviewProvider {
+    static var previews: some View {
+      SocialLoginViewController().toPreview()
+    }
 }
