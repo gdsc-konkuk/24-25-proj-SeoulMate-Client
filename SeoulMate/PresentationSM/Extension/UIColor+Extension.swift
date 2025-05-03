8432325926f708a322f341c16e531d7eef42d113
ParkSeongGeun <phd0801@naver.com>
2025-05-03 09:22:24 +0900

    Create UIColor+Extension.swift

---
+++ SeoulMate/PresentationSM/Extension/UIColor+Extension.swift
@@ -0,0 +1,143 @@
+//
+//  UIColor+Extension.swift
+//  SeoulMate
+//
+//  Created by 박성근 on 5/3/25.
+//
+
+import UIKit
+
+extension UIColor {
+  static let gray50 = UIColor(
+    red: 249 / 255,
+    green: 249 / 255,
+    blue: 249 / 255,
+    alpha: 1
+  )
+  
+  static let gray100 = UIColor(
+    red: 240 / 255,
+    green: 240 / 255,
+    blue: 241 / 255,
+    alpha: 1
+  )
+  
+  static let gray200 = UIColor(
+    red: 225 / 255,
+    green: 225 / 255,
+    blue: 227 / 255,
+    alpha: 1
+  )
+  
+  static let gray300 = UIColor(
+    red: 210 / 255,
+    green: 210 / 255,
+    blue: 214 / 255,
+    alpha: 1
+  )
+  
+  static let gray400 = UIColor(
+    red: 180 / 255,
+    green: 180 / 255,
+    blue: 187 / 255,
+    alpha: 1
+  )
+  
+  static let gray500 = UIColor(
+    red: 150 / 255,
+    green: 150 / 255,
+    blue: 160 / 255,
+    alpha: 1
+  )
+  
+  static let gray600 = UIColor(
+    red: 90 / 255,
+    green: 91 / 255,
+    blue: 106 / 255,
+    alpha: 1
+  )
+  
+  static let gray700 = UIColor(
+    red: 58 / 255,
+    green: 58 / 255,
+    blue: 68 / 255,
+    alpha: 1
+  )
+  
+  static let gray800 = UIColor(
+    red: 41 / 255,
+    green: 42 / 255,
+    blue: 49 / 255,
+    alpha: 1
+  )
+  
+  static let gray900 = UIColor(
+    red: 25 / 255,
+    green: 25 / 255,
+    blue: 29 / 255,
+    alpha: 1
+  )
+  
+  static let main50 = UIColor(
+    red: 217 / 255,
+    green: 217 / 255,
+    blue: 217 / 255,
+    alpha: 1
+  )
+  
+  static let main100 = UIColor(
+    red: 212 / 255,
+    green: 236 / 255,
+    blue: 255 / 255,
+    alpha: 1
+  )
+  
+  static let main200 = UIColor(
+    red: 159 / 255,
+    green: 213 / 255,
+    blue: 255 / 255,
+    alpha: 1
+  )
+  
+  static let main300 = UIColor(
+    red: 106 / 255,
+    green: 191 / 255,
+    blue: 255 / 255,
+    alpha: 1
+  )
+  
+  static let main400 = UIColor(
+    red: 53 / 255,
+    green: 168 / 255,
+    blue: 254 / 255,
+    alpha: 1
+  )
+  
+  static let main500 = UIColor(
+    red: 0 / 255,
+    green: 145 / 255,
+    blue: 254 / 255,
+    alpha: 1
+  )
+  
+  static let red = UIColor(
+    red: 237 / 255,
+    green: 4 / 255,
+    blue: 4 / 255,
+    alpha: 1
+  )
+  
+  static let blue = UIColor(
+    red: 35 / 255,
+    green: 135 / 255,
+    blue: 242 / 255,
+    alpha: 1
+  )
+  
+  static let yellow = UIColor(
+    red: 252 / 255,
+    green: 187 / 255,
+    blue: 0 / 255,
+    alpha: 1
+  )
+}
